1ea3db5f61463f1b746d17a4fa2843e46c3820cd
Michelle Tomaselli <michelletomaselli@MacBook-Pro-de-Michelle.local>
2018-10-03 13:45:18 +0200

    Explain script

--- seqClass.sh
+++ seqClass.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
-seq=$1
-seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
-if [[ $seq =~ ^[ACGTU]+$ ]]; then
-  if [[ $seq =~ T ]]; then
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then
-    echo "The sequence is RNA"
-  else
-    echo "The sequence can be DNA or RNA"
+#!/bin/bash #Linea informativa
+seq=$1 #Guarda el valor a entrar como argumento en una variable a analizar
+seq=$(echo $seq | tr a-z A-Z)  #Cambia los argumentos entrados a mayuscula
+if [[ $seq =~ ^[ACGTU]+$ ]]; then #Si el valor entrado corresponde a ACGTU entrara dentro del condicional
+  if [[ $seq =~ T ]]; then #Si tiene T entrara dentro
+    echo "The sequence is DNA" #Te retornara el mensaje 
+  elif [[ $seq =~ U ]]; then #Si tiene U entrara dentro
+    echo "The sequence is RNA" #Te retornara el mensaje
+  else  #Si no contiene T o U el argumento, entrara dentro
+    echo "The sequence can be DNA or RNA" #Te retornara el mensaje
   fi
-else
-  echo "The sequence is not DNA nor RNA"
-fi
+else #Si no contiene ninguno de los valores dichos anteriormente ejecutara este condicional
+  echo "The sequence is not DNA nor RNA" #Te retornara el mensaje
+fi #Acaba el script
+
