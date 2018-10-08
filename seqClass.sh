54c1edc689532de50642bd72862dff3a3a31f27a
Michelle Tomaselli <michelletomaselli@MacBook-Pro-de-Michelle.local>
2018-10-08 15:49:53 +0200

    git_HandsOn Exercise 1

--- seqClass.sh
+++ seqClass.sh
@@ -3,8 +3,12 @@
 seq=$1 #Guarda el valor a entrar como argumento en una variable a analizar
 seq=$(echo $seq | tr a-z A-Z)  #Cambia los argumentos entrados a mayuscula
 if [[ $seq =~ ^[ACGTU]+$ ]]; then #Si el valor entrado corresponde a ACGTU entrara dentro del condicional
-  if [[ $seq =~ T ]]; then #Si tiene T entrara dentro
-    echo "The sequence is DNA" #Te retornara el mensaje 
+  if [[ $seq =~ T ]]; then #Si contiene alguna T entrara dentro
+    if [[ $seq =~ U ]]; then #Si ademas de la T contiene una U entrara dentro     
+      echo "The sequence can be DNA or RNA" #Te retornara el mensaje 
+    else #Si no tiene ninguna U pero si T ya que esta dentro de este condicional, entrara dentro
+      echo "The sequence is DNA" #Te retornara el mensaje #Te retornara el mensaje
+    fi 
   elif [[ $seq =~ U ]]; then #Si tiene U entrara dentro
     echo "The sequence is RNA" #Te retornara el mensaje
   else  #Si no contiene T o U el argumento, entrara dentro
